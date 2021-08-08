c97d76e84daf608d83fe9513eca7fd594d011047
kellydhunter1 <kellydhunter1@gmail.com>
2021-08-08 13:39:28 -0400

    fetch route for city and current date added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,25 @@
 const apiKey="31c5c2a5281cccdbc583978a1e5bc66d";
 
-const displayWeather = function(cityName, state) { 
-    const apiUrl = fetch("https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "," + state +"&appid=" + apiKey);
-console.log(apiUrl);
+const displayWeather = function(city, state) { 
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + ",us&APPID=" + apiKey;
+    const date = new Date();
+    fetch(apiUrl).then(function(response) {
+        if (response.ok) {
+            response.json().then(function(data) {
+                console.log("Here are the results ", data.name, date.getMonth() + "/" + date.getDate() +"/" + date.getFullYear());
+            })
+        }
+    })
+    console.log(apiUrl);
+
+
+
 };
 
-const citySearch = function(cityName, state) {
-    cityName = "Richmond";
-    state = "IN";
-    displayWeather(cityName, state);
+const citySearch = function(city, state) {
+    city = "Chicago";
+    state = "il";
+    displayWeather(city, state);
 };
 citySearch();
 
