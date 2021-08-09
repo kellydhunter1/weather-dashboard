acdf1cb932f0664a825ddbb9270af835c00904bf
kellydhunter1 <kellydhunter1@gmail.com>
2021-08-08 20:26:55 -0400

    searched cities saves to local storage and recalls on click

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,17 +8,23 @@ const iconImg = document.createElement("img");
 const currentWindEl = document.querySelector("#wind");
 const currentHumidityEl = document.querySelector("#humidity");
 const currentUvIndexEl = document.querySelector("#uv-index");
+const recentCitiesEl = document.querySelector("#recent-cities");
+const cityInput = document.querySelector("#city");
+const stateInput = document.querySelector("#state");
+const citySearchBtn = document.querySelector("#city-search");
 const date = new Date();
+let savedCitiesArr = [];
+let apiUrl = "";
 
 
-const findWeatherInfo = function(city, state) { 
-    const apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + ",us&units=imperial&APPID=" + apiKey;
-    
+const findWeatherInfo = function(apiUrl) { 
     fetch(apiUrl).then(function(response) {
         if (response.ok) {
             response.json().then(function(data) {
                 console.log("Here are the results for ", data.name, date.getMonth() + "/" + date.getDate() +"/" + date.getFullYear());
                 displayWeather(data);
+                saveCity(data);
+                recentCityList(data);
             });
         } else {
             alert('Error: City Not Found');
@@ -36,15 +42,67 @@ const displayWeather = function(data) {
     iconImg.setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + ".png");
     iconEl.appendChild(iconImg);
     currentUvIndexEl.textContent="UV Index: ";
-    console.log("City: " + data.name, "Temp: " + data.main.temp + "°F","Humidity: " + data.main.humidity + "%", "Wind: " + data.wind.speed + " MPH")
-
-}
-
-const citySearch = function(city, state) {
-    city = "Chicago";
-    state = "il";
-    findWeatherInfo(city, state);
+    console.log("City: " + data.name, "Temp: " + data.main.temp + "°F","Humidity: " + data.main.humidity + "%", "Wind: " + data.wind.speed + " MPH");
+    
 };
-citySearch();
 
+const recentCityList = function(data) {
+    const recentCityBtn = document.createElement("button");
+    recentCityBtn.setAttribute("id", data.id);
+    recentCityBtn.setAttribute("type", "button");
+    recentCityBtn.classList= "btn btn-info recent-city";
+    recentCityBtn.textContent = data.name;
+    recentCitiesEl.appendChild(recentCityBtn);
+};
 
+const cityRecallHandler = function(event) {
+    if(event.target.matches(".recent-city")) {
+    event.preventDefault();
+    let cityId = event.target.getAttribute("id");
+    cityRecall(cityId);
+    }
+};
+
+const cityRecall = function(cityId) {
+    let apiUrl = "http://api.openweathermap.org/data/2.5/weather?id=" + cityId + "&units=imperial&appid=" + apiKey;
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data){
+            displayWeather(data);
+        })
+    })
+};
+const saveCity = function(data) {
+    const savedCity = {
+        name: data.name,
+        id: data.id
+    };  
+    savedCitiesArr.push(savedCity);    
+
+    localStorage.setItem("saved cities", JSON.stringify(savedCitiesArr));
+    console.log(data.name + " saved to local storage!");
+};
+
+const loadCities = function() {
+    let savedCities = JSON.parse(localStorage.getItem("saved cities"));
+        if(!savedCities) {
+            return false
+        } else {
+            console.log("Saved Cities Found!");
+            for (let i = 0; i < savedCities.length; i++) {
+                recentCityList(savedCities[i]);
+                savedCitiesArr = savedCities;          
+            }
+
+        }
+};
+
+const citySearch = function() {
+    city = cityInput.value;
+    state = stateInput.value;
+    let apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "," + state + ",us&units=imperial&APPID=" + apiKey;
+    findWeatherInfo(apiUrl);
+};
+recentCitiesEl.addEventListener("click", cityRecallHandler);
+citySearchBtn.addEventListener("click", citySearch)
+
+loadCities();
